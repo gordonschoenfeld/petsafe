b17507ab6c5ffe33eaeb37473ca14973b54ea7df
Gordon Schoenfeld <gordon.schoenfeld@gmail.com>
2026-02-14 15:11:31 -0800

    Finalized wizards

--- setup/setup_wizard.py
+++ setup/setup_wizard.py
@@ -2,19 +2,6 @@
 
 import subprocess
 
-# ...FYI on how to run...
-# # Execution with arguments
-# subprocess.run(["python3", "other_script.py", "arg1", "--flag"])
-
-# # Capture the output to a variable
-# result = subprocess.run(
-#     ["python3", "other_script.py"],
-#     capture_output=True,
-#     text=True
-# )
-# print(f"Script output: {result.stdout}")
-
-
 # Run setup_auth.py
 subprocess.run(["python3", "setup/setup_auth.py"])
 
